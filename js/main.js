222cac3706d26380fe3850a7ae7312af7ac49232
Calvin Jung <ediyacoo@gmail.com>
2013-04-21 21:29:14 +0300

    Some idea about the layout of socialMedia.

--- js/main.js
+++ js/main.js
@@ -683,7 +683,7 @@ function showLocalInfo(id){
 	var locationX = location.coordinate.x;
 	var locationY = location.coordinate.y;
 	var $select_media=$("#localInfo_socialMedia");
-	$select_media.html("<br/>Lat: <input type='text' id=lat value=" + locationX + "> <br/>Long: <input type='text' id=lng value=" + locationY + "> <br/>Keyword: <input type='text' id='keyword' value='shoes'><br><button type='button' onclick='callPython()'>Search</button>");
+	//$select_media.html("<br/>Lat: <input type='text' id=lat value=" + locationX + "> <br/>Long: <input type='text' id=lng value=" + locationY + "> <br/>Keyword: <input type='text' id='keyword' value='shoes'><br><button type='button' onclick='callPython()'>Search</button>");
 	
 	
 
