7a74372291c0e6dc9402521a4a67827a7c67658a
Chris Allen <cccchris.allen@gmail.com>
2013-05-03 16:52:35 -0700

    Improving geocoder

--- retrieveGeocodedTable.py
+++ retrieveGeocodedTable.py
@@ -7,6 +7,7 @@ cgitb.enable()
 
 form = cgi.FieldStorage()
 fname = form['fileName'].value
+geoColumn = form['geoColumn'].value
 
 geoRows = pickle.load(open(os.path.abspath(__file__).replace(__file__, fname + ".p")))
 
