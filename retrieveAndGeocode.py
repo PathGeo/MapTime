d5aa727f1e2f21ace406f6b08b8786bf281cddf8
Chris Allen <cccchris.allen@gmail.com>
2013-05-12 23:44:23 -0700

    Now limiting GeoJSON properties (for demo)...
    Note: Comment out lines 47-49 to return ALL properties.

--- retrieveAndGeocode.py
+++ retrieveAndGeocode.py
@@ -43,11 +43,11 @@ def geocodeRows(rows, locFunc):
 			lat, lon = locFunc(row)
 			if lat and lon:			
 				#NOTE: This is just a temporary workaround for the problem with DataTables (can't display a lot of columns)
-				'''
+				#Comment out lines 47-49 to return all properties
 				if len(row.keys()) > 5:
 					for key in row.keys()[5:]:
 						del row[key]
-				'''
+				
 				#End Note
 				doc = dict(type='Feature', geometry=dict(type="Point", coordinates=[lon, lat]), properties=row.copy())
 				features.append(doc)
