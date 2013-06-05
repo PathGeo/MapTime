36c498d5fef7d345887aa0f62c5a1cd408991249
Calvin Jung <ediyacoo@gmail.com>
2013-06-05 22:45:20 +0300

    UI: SocialMedia Source

--- js/social_media.js
+++ js/social_media.js
@@ -51,8 +51,9 @@ function callPython(inputValue){
 		//var ts = (Math.floor(Date.now()/1000)) - (document.getElementById("socialMedia_temporal").value);
 		var ts = (Math.floor(Date.now()/1000)) - (63072000);
 		//var source = document.getElementById("socialMedia_source").value;
-		var source = $("#socialMedia_source li img[selected=selected]").attr("value");
-		
+		//var source = $("#socialMedia_source li img[selected=selected]").attr("value");
+		source= $("#socialMedia_search .ui-radio .ui-btn-active").siblings('input').val();
+	
 		
 		console.log(lat);
 		console.log(lng);
