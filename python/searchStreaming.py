0a2beed6908d7c6f53e38b70ff0427158ed063e6
Calvin Jung <ediyacoo@gmail.com>
2014-05-15 18:04:41 -0700

    add hideColumns parameters

--- python/searchStreaming.py
+++ python/searchStreaming.py
@@ -12,7 +12,7 @@ def getURLParameter(key, cgiFieldStorage):
 
 
 #convert to geojson
-def convertToGeojson(obj_array, latFieldName, lonFieldName):
+def convertToGeojson(obj_array, latFieldName, lonFieldName, hideColumns=None):
         import json
         
         dthandler = lambda obj: (obj.isoformat() if isinstance(obj, datetime) or isinstance(obj, date) else None)
@@ -22,6 +22,10 @@ def convertToGeojson(obj_array, latFieldName, lonFieldName):
                 lat=obj[latFieldName]
                 lon=obj[lonFieldName]
 
+                if(hideColumns):
+                      for column in hideColumns:
+                            obj.pop(column,"None")
+                
                 results.append(dict(type="Feature", geometry=dict(type="Point", coordinates=[lon,lat]), properties=obj))
 
         return json.dumps({"type":"FeatureCollection", "count":obj_array.count(), "features":results}, default=dthandler)
@@ -36,11 +40,13 @@ keywords=getURLParameter('keywords', params)
 dateFrom=getURLParameter('dateFrom', params)
 dateTo=getURLParameter('dateTo', params)
 bbox=getURLParameter('bbox', params)
+hideColumns=getURLParameter('hideColumns', params)
 
 #change data type
 dateFrom=datetime.strptime(dateFrom, "%Y-%m-%d")
 dateTo=datetime.strptime(dateTo, "%Y-%m-%d")
 keywords=str(keywords).split(",")
+hideColumns=str(hideColumns).split(",")
 
 #change keywords to regex
 import re
@@ -58,4 +64,4 @@ tweets=col.find(query)
 
 #parse tweet
 	
-print convertToGeojson(tweets, 'lat', 'lon')
+print convertToGeojson(tweets, 'lat', 'lon', hideColumns=hideColumns)
